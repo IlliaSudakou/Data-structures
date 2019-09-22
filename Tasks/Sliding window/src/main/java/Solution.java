cc747bf6bc0e432a33427b2949dbca0d7bad2d7b
Illia Sudakou <Illia.Sudakou@datalex.com>
2019-09-22 14:07:45 +0300

    sliding window re-implementation

--- Tasks/Sliding window/src/main/java/Solution.java
+++ Tasks/Sliding window/src/main/java/Solution.java
@@ -3,35 +3,13 @@ import java.util.*;
 /*
         Input : n numbers, m - window length
             go through numbers using m window
-        Output: minimum for each window
+        Output: maximum / minimum for each window
 
         Example: 1 - 2 - 4 - 8 - 9
                 Window: 3 (first window 1 - 2 - 4; second 2 - 4 - 8 etc.)
                 Output: 1 - 2 - 4
  */
 public class Solution {
-    /**
-     * First solution with foreach and sorting
-     *
-     * @param n numbers
-     * @param m window length
-     * @return minimums
-     */
-    public static int[] getWindowsSimple(int[] n, int m){
-        List<Integer> result = new ArrayList<>();
-        if (m == 1 || n.length == 1){
-            return n;
-        }
-        int windowsCount = n.length - m + 1;
-        int windowTo = m - 1;
-        for (int i = 0; i < windowsCount; i++, windowTo++) {
-            int[] window = Arrays.copyOfRange(n, i, windowTo + 1);
-            Arrays.sort(window);
-            result.add(window[0]);
-        }
-        return result.stream().mapToInt(i->i).toArray();
-    }
-
 
     /**
      * Second solution with queue
@@ -40,34 +18,122 @@ public class Solution {
      * @param m window length
      * @return minimums
      */
-    public static int[] getWindowsQueque(int[] n, int m){
+    public static int[] getWindowsQuequeMin(int[] n, int m){
         List<Integer> result = new ArrayList<>();
-
         if (m == 1 || n.length == 1){
             return n;
         }
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> rightStack = new Stack<>();
 
-        Queue<Integer> windowQueue = new LinkedList<>();
+        Stack<Integer> leftStackMin = new Stack<>();
+        Stack<Integer> rightStackMin = new Stack<>();
 
         for (int i = 0; i < m; i++) {
-            windowQueue.add(n[i]);
+            populateStackMin(leftStack, leftStackMin, n[i]);
         }
-        int currentMinimum = Collections.min(windowQueue);
-        result.add(currentMinimum);
+        populateRightStackMin(result, leftStack, rightStack, rightStackMin, leftStackMin);
 
         for (int i = m; i < n.length; i++) {
-            windowQueue.add(n[i]);
-            if (n[i] <= currentMinimum){
-                currentMinimum = n[i];
-                result.add(currentMinimum);
-                windowQueue.poll();
-                continue;
+            populateStackMin(leftStack, leftStackMin, n[i]);
+            if (!rightStack.isEmpty()){
+                rightStack.pop();
+                rightStackMin.pop();
+                if (rightStack.isEmpty()){
+                    populateRightStackMin(result, leftStack, rightStack, rightStackMin, leftStackMin);
+                }
+                else {
+                    if (leftStackMin.peek() < rightStackMin.peek()){
+                        result.add(leftStackMin.peek());
+                    }
+                    else {
+                        result.add(rightStackMin.peek());
+                    }
+                }
             }
-            if (windowQueue.poll().equals(currentMinimum)){
-                currentMinimum = Collections.min(windowQueue);
+            else {
+                populateRightStackMin(result, leftStack, rightStack, rightStackMin, leftStackMin);
             }
-            result.add(currentMinimum);
         }
         return result.stream().mapToInt(i->i).toArray();
+    }
+
+
+    public static int[] getWindowsQuequeMaximum(int[] n, int m){
+        List<Integer> result = new ArrayList<>();
+        if (m == 1 || n.length == 1){
+            return n;
+        }
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> rightStack = new Stack<>();
+
+        Stack<Integer> leftStackMax = new Stack<>();
+        Stack<Integer> rightStackMax = new Stack<>();
+
+        for (int i = 0; i < m; i++) {
+            populateStackMax(leftStack, leftStackMax, n[i]);
+        }
+        populateRightStack(result, leftStack, rightStack, rightStackMax, leftStackMax);
+
+        for (int i = m; i < n.length; i++) {
+            populateStackMax(leftStack, leftStackMax, n[i]);
+            if (!rightStack.isEmpty()){
+                rightStack.pop();
+                rightStackMax.pop();
+                if (rightStack.isEmpty()){
+                    populateRightStack(result, leftStack, rightStack, rightStackMax, leftStackMax);
+                }
+                else {
+                    if (leftStackMax.peek() > rightStackMax.peek()){
+                        result.add(leftStackMax.peek());
+                    }
+                    else {
+                        result.add(rightStackMax.peek());
+                    }
+                }
+            }
+            else {
+                populateRightStack(result, leftStack, rightStack, rightStackMax, leftStackMax);
+            }
+        }
+        return result.stream().mapToInt(i->i).toArray();
+    }
+
+    private static void populateStackMax(Stack<Integer> leftStack, Stack<Integer> leftStackMax, int currentValue) {
+        leftStack.push(currentValue);
+        if (leftStackMax.isEmpty() || leftStackMax.peek() < currentValue) {
+            leftStackMax.push(currentValue);
+        } else {
+            leftStackMax.push(leftStackMax.peek());
+        }
+    }
+
+    private static void populateRightStack(List<Integer> result, Stack<Integer> leftStack,
+                                           Stack<Integer> rightStack, Stack<Integer> rightStackMax, Stack<Integer> leftStackMax) {
+        while (!leftStack.isEmpty()) {
+            Integer item = leftStack.pop();
+            leftStackMax.pop();
+            populateStackMax(rightStack, rightStackMax, item);
+        }
+        result.add(rightStackMax.peek());
+    }
+
+    private static void populateStackMin(Stack<Integer> leftStack, Stack<Integer> leftStackMin, int currentValue) {
+        leftStack.push(currentValue);
+        if (leftStackMin.isEmpty() || leftStackMin.peek() > currentValue) {
+            leftStackMin.push(currentValue);
+        } else {
+            leftStackMin.push(leftStackMin.peek());
+        }
+    }
+
+    private static void populateRightStackMin(List<Integer> result, Stack<Integer> leftStack,
+                                           Stack<Integer> rightStack, Stack<Integer> rightStackMin, Stack<Integer> leftStackMin) {
+        while (!leftStack.isEmpty()) {
+            Integer item = leftStack.pop();
+            leftStackMin.pop();
+            populateStackMin(rightStack, rightStackMin, item);
+        }
+        result.add(rightStackMin.peek());
     }
 }
--- Tasks/Sliding window/src/test/java/SolutionTest.java
+++ Tasks/Sliding window/src/test/java/SolutionTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,26 +11,41 @@ public class SolutionTest {
 
     @ParameterizedTest(name="Run {index}: n={0}, window={1}, expectedValue={2}")
     @MethodSource("testParameters")
-    public void testFirstSolution(int[] n, int window, int[] expectedValue) {
+    public void testSecondSolution(int[] n, int window, int[] expectedValue) {
 
-        assertArrayEquals(expectedValue, Solution.getWindowsSimple(n, window));
+        assertArrayEquals(expectedValue, Solution.getWindowsQuequeMin(n, window));
     }
 
     @ParameterizedTest(name="Run {index}: n={0}, window={1}, expectedValue={2}")
-    @MethodSource("testParameters")
-    public void testSecondSolution(int[] n, int window, int[] expectedValue) {
+    @MethodSource("testParametersMax")
+    public void testSecondSolutionMax(int[] n, int window, int[] expectedValue) {
 
-        assertArrayEquals(expectedValue, Solution.getWindowsQueque(n, window));
+        assertArrayEquals(expectedValue, Solution.getWindowsQuequeMaximum(n, window));
     }
 
     static Stream<Arguments> testParameters() throws Throwable {
         return Stream.of(
+                Arguments.of(new int[] {2,7,3,1,5,2,6,2}, 4, new int[] {1,1,1,1,2} ),
                 Arguments.of(new int[] {1,2,4,8,9}, 3, new int[] {1,2,4} ),
                 Arguments.of(new int[] {1}, 1, new int[] {1} ),
                 Arguments.of(new int[] {1,2,4,8,9}, 1, new int[] {1,2,4,8,9} ),
                 Arguments.of(new int[] {100,99,98,97,96,95,94,93,90,85,80,75,70,65,60,55,50,45,40,35,30,25,20,15,10},
                         3, new int[] {98,97,96,95,94,93,90,85,80,75,70,65,60,55,50,45,40,35,30,25,20,15,10} ),
                 Arguments.of(new int[] {1,2,4,8,9,0,5,1,1,1,1,1}, 8, new int[] {0,0,0,0,0} ),
+                Arguments.of(new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                        2, new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1} )
+        );
+    }
+
+    static Stream<Arguments> testParametersMax() throws Throwable {
+        return Stream.of(
+                Arguments.of(new int[] {2,7,3,1,5,2,6,2}, 4, new int[] {7,7,5,6,6} ),
+                Arguments.of(new int[] {1,2,4,8,9}, 3, new int[] {4,8,9} ),
+                Arguments.of(new int[] {1}, 1, new int[] {1} ),
+                Arguments.of(new int[] {1,2,4,8,9}, 1, new int[] {1,2,4,8,9} ),
+                Arguments.of(new int[] {100,99,98,97,96,95,94,93,90,85,80,75,70,65,60,55,50,45,40,35,30,25,20,15,10},
+                        3, new int[] {100,99,98,97,96,95,94,93,90,85,80,75,70,65,60,55,50,45,40,35,30,25,20} ),
+                Arguments.of(new int[] {1,2,4,8,9,0,5,1,1,1,1,1}, 8, new int[] {9,9,9,9,9} ),
                 Arguments.of(new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                         2, new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1} )
         );
